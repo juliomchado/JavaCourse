56dccd9d84a5a559bfe40b971f616b2ab4d0ec07
Julio Machado <juliocarlos00@hotmail.com>
2020-12-10 08:43:03 -0300

    output data with java

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args){
+
+        String nome = "Maria";
+        int idade = 31;
+        double renda = 4000.0;
+        
+        int y = 32;
+        double x = 10.35784;
+        
+        System.out.println(y);
+        System.out.printf("%.2f%n", x);
+        System.out.printf("%.4f%n", x);
+        Locale.setDefault(Locale.US);
+        System.out.println("Bom dia!");
+        System.out.printf("RESULTADO = " + x + " METROS%n");
+        System.out.printf("RESULTADO = %.2f METROS%n", x);
+        System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade, renda);
+
+
+    }
+}
