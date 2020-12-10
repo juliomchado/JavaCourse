f8a5eaf15da26d901933742ca29e2632ed00f5b9
Julio Machado <juliocarlos00@hotmail.com>
2020-12-10 09:18:34 -0300

    input in java

--- Challenges/src/Main.java
+++ Challenges/src/Main.java
@@ -1,28 +1,29 @@
-import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        String product1 = "Computer";
-        String product2 = "Office desk";
 
-        int age = 30;
-        int code = 5290;
-        char gender = 'F';
+        Scanner sc = new Scanner(System.in);
 
-        double price1 = 2100.0;
-        double price2 = 650.50;
-        double measure = 53.234567;
-
-        Locale.setDefault(Locale.US);
-
-        System.out.printf("%s, which price is $ %.2f%n", product1, price1);
-        System.out.printf("%s, which price is $ %.2f%n", product2, price2);
+        String x;
+        x = sc.next();
+        System.out.println("1. Você digitou: " + x);
         
-        System.out.printf("%nRecord: %d years old, code %d and gender: %c%n", age, code, gender);
-   
-        System.out.printf("%nMeasue with eight decimal places: %.8f%n", measure);
-        System.out.printf("Rouded (three decimal places): %.3f%n", measure);
-        System.out.printf("US decimal point: %.3f%n", measure);
+        
+        int y;
+        y = sc.nextInt();
+        System.out.println("2. Você digitou: " + y);
+
+
+        double l;
+        l = sc.nextDouble();
+        System.out.printf("3. Você digitou: %.2f%n", l);
+
+        char z;
+        z = sc.next().charAt(0);
+        System.out.println("4. Você digitou: " + z);
+
+        sc.close();
     }
 }
