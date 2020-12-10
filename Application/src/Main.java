61e77609d0ffdd8ca4c5851ee987d817957e6563
Julio Machado <juliocarlos00@hotmail.com>
2020-12-10 13:16:48 -0300

    methods in java

--- Application/src/Main.java
+++ Application/src/Main.java
@@ -3,48 +3,36 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-    
-        // Formatar: toLowerCase(), toUpperCase(), trim()
-        // Recortar: substring(inicio), substring(inicio, fim)
-        // Substituir: Replace(char, char), Replace(string, string)
-        // Buscar: IndexOf, LastIndexOf
-        // str.Split("")
+        Scanner sc = new Scanner(System.in);
 
-        String original = "abcde FGHIJ ABC abc DEFG";
-        String s = "potato apple lemon";
+        System.out.println("Enter three numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
 
-        String s01 = original.toLowerCase();
-        String s02 = original.toUpperCase();
-        String s03 = original.trim();
-        String s04 = original.substring(2);
-        String s05 = original.substring(2, 9);
-        String s06 = original.replace('a', 'x');
-        String s07 = original.replace("abc", "xy");
-        
-        int i = original.indexOf("bc");
-        int j = original.lastIndexOf("bc");
-        String[] vect = s.split(" ");
+        int higher = max(a, b, c);
 
-        System.out.println("Original: -"+ original + "-");
-        System.out.println("toLowerCase: -" + s01 + "-");
-        System.out.println("toUpperCase: -" + s02 + "-");
-        System.out.println("trim: -" + s03 + "-");
+        showResult(higher);
 
-        System.out.println();
+        sc.close();
 
-        System.out.println("substring(2): -" + s04 + "-");
-        System.out.println("substring(2, 9): -" + s05 + "-");
-        System.out.println("replace('a', 'x'): -" +  s06 + "-");
-        System.out.println("replace('abc', 'xy'): -" +  s07 + "-");
+    }
 
-        System.out.println("Index of 'bc': " +  i);
-        System.out.println("Last index of 'bc': "+  j);
+    public static int max(int x, int y, int z) {
+        int aux;
 
-        System.out.println();
+        if (x > y && x > z) {
+            aux = x;
+        } else if (y > z) {
+            aux = y;
+        } else {
+            aux = z;
+        }
 
-        System.out.println(vect[0]);
-        System.out.println(vect[1]);
-        System.out.println(vect[2]);
-        
+        return aux;
+    }
+
+    public static void showResult(int value) {
+        System.out.println("Higher = " + value);
     }
 }
