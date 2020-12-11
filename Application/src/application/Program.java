2c278a03a05f339e4becf807b24861e391174e9c
Julio Machado <juliocarlos00@hotmail.com>
2020-12-11 08:13:54 -0300

    vector with reference tipe (classe)

--- Application/src/application/Program.java
+++ Application/src/application/Program.java
@@ -10,21 +10,24 @@ public class Program {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        double[] vect = new double[n];
+        Product[] vect = new Product[n];
 
-        for (int i = 0; i < n; i++) {
-            vect[i] = sc.nextDouble();
+        for (int i = 0; i < vect.length; i++) {
+            sc.nextLine();
+            String name = sc.nextLine();
+            double price = sc.nextDouble();
+            vect[i] = new Product(name, price);
         }
 
-        double sum = 0.0;
-
-        for (int i = 0; i < n; i++) {
-            sum += vect[i];
+        double sum = 0;
+        
+        for(int i = 0; i< vect.length;i++){
+            sum += vect[i].getPrice();
         }
 
-        double avg = sum / n;
+        double avg = sum / vect.length;
 
-        System.out.printf("AVARAGE HEIGHT: %.2f%n", avg);
+        System.out.printf("AVERAGE PRICE = %.2f%n",avg);
 
         sc.close();
     }
--- Application/src/entities/Product.java
+++ Application/src/entities/Product.java
@@ -1,26 +1,21 @@
 package entities;
 
 public class Product {
+    
     private String name;
     private double price;
-    private int quantity;
+
 
     public Product() {
     }
 
-    public Product(String name, double price, int quantity) {
-        this.name = name;
-        this.price = price;
-        this.quantity = quantity;
-    }
-    
     public Product(String name, double price) {
         this.name = name;
         this.price = price;
     }
 
     public String getName() {
-        return name;
+        return this.name;
     }
 
     public void setName(String name) {
@@ -28,31 +23,22 @@ public class Product {
     }
 
     public double getPrice() {
-        return price;
+        return this.price;
     }
 
     public void setPrice(double price) {
         this.price = price;
     }
 
-    public int getQuantity() {
-        return quantity;
+    public Product name(String name) {
+        this.name = name;
+        return this;
     }
 
-    public double totalValueInStock() {
-        return price * quantity;
+    public Product price(double price) {
+        this.price = price;
+        return this;
     }
-
-    public void addProducts(int quantity) {
-        this.quantity += quantity;
-    }
-
-    public void removeProducts(int quantity) {
-        this.quantity -= quantity;
-    }
-
-    public String toString() {
-        return name + ", $ " + String.format("%.2f", price) + ", " + quantity + " units, Total: $ "
-                + String.format("%.2f", totalValueInStock());
-    }
+    
+    
 }
