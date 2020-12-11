a20537cd37c44cf6ad0e0a0602327edcae79a89f
Julio Machado <juliocarlos00@hotmail.com>
2020-12-11 08:52:12 -0300

    array challenge

---
+++ Challenges/src/application/Program.java
@@ -0,0 +1,34 @@
+package application;
+
+import java.text.ParseException;
+import java.util.Scanner;
+import entities.Students;
+
+public class Program {
+    public static void main(String[] args) throws ParseException {
+        Scanner sc = new Scanner(System.in);
+        Students[] vect = new Students[10];
+        System.out.print("How many rooms will be Studentsed? ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            System.out.println();
+            System.out.println("Students #" + i + ":");
+            System.out.print("Name: ");
+            sc.nextLine();
+            String name = sc.nextLine();
+            System.out.print("Email: ");
+            String email = sc.nextLine();
+            System.out.print("Room: ");
+            int room = sc.nextInt();
+            vect[room] = new Students(name, email);
+        }
+        System.out.println();
+        System.out.println("Busy rooms:");
+        for (int i = 0; i < 10; i++) {
+            if (vect[i] != null) {
+                System.out.println(i + ": " + vect[i]);
+            }
+        }
+        sc.close();
+    }
+}
