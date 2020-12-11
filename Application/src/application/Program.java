fce2888f50a3cd64e9f40e0904195cb170ad9c84
Julio Machado <juliocarlos00@hotmail.com>
2020-12-11 12:40:56 -0300

    data format

--- Application/src/application/Program.java
+++ Application/src/application/Program.java
@@ -1,51 +1,42 @@
 package application;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class Program {
-    public static void main(String[] args) {
-        List<String> list = new ArrayList<>();
-    
-        list.add("Maria");
-        list.add("Alex");
-        list.add("Bob");
-        list.add("Anna");
-        list.add(2, "Marco");
+    public static void main(String[] args) throws ParseException {
+        SimpleDateFormat sdf1 = new SimpleDateFormat("dd/MM/yyyy");
+        SimpleDateFormat sdf2 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        SimpleDateFormat sdf3 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        sdf3.setTimeZone((TimeZone.getTimeZone("GMT")));
+
+        Date x1 = new Date();
+        Date x2 = new Date(System.currentTimeMillis());
+        Date x3 = new Date(0L);
+        Date x4 = new Date(1000L * 60L * 60L * 5L);
 
 
-        System.out.println(list.size());
-
-        list.remove(1);
-
-        for(String x : list){
-            System.out.println(x);
-        }
-
-        System.out.println("------------");
-        list.removeIf(x -> x.charAt(0) == 'M');
-
-        for(String x : list){
-            System.out.println(x);
-        }
-
-        System.out.println("------------");
-        System.out.println("Index of Bob: " + list.indexOf("Bob"));
-        System.out.println("Index of Marco: " + list.indexOf("Marco"));
-        System.out.println("------------");
-
-        List<String> result = list.stream().filter(x -> x.charAt(0) == 'A').collect(Collectors.toList());
-
-        for(String x : result){
-            System.out.println(x);
-        }
-        System.out.println("------------");
-
-        String name = list.stream().filter(x -> x.charAt(0) == 'A').findFirst().orElse(null);
-        System.out.println(name);
-
+        Date y1 = sdf1.parse("25/06/2018");
+        Date y2 = sdf2.parse("25/06/2018 15:42:07");
+        Date y3 = Date.from(Instant.parse("2018-06-25T15:42:07Z"));
+        System.out.println("x1: " + sdf2.format(x1));
+        System.out.println("x2: " + sdf2.format(x2));
+        System.out.println("x3: " + sdf2.format(x3));
+        System.out.println("x4: " + sdf2.format(x4));
+        System.out.println("y1: " + sdf2.format(y1));
+        System.out.println("y2: " + sdf2.format(y2));
+        System.out.println("y3: " + sdf2.format(y3));
+        System.out.println("-------------");
+        System.out.println("x1: " + sdf3.format(x1));
+        System.out.println("x2: " + sdf3.format(x2));
+        System.out.println("x3: " + sdf3.format(x3));
+        System.out.println("x4: " + sdf3.format(x4));
+        System.out.println("y1: " + sdf3.format(y1));
+        System.out.println("y2: " + sdf3.format(y2));
+        System.out.println("y3: " + sdf3.format(y3));
 
     }
 }
