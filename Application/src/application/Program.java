b5fae15b5a6f4d0937e0ecc1190617618f44f23f
Julio Machado <juliocarlos00@hotmail.com>
2020-12-11 09:15:05 -0300

    List

--- Application/src/application/Program.java
+++ Application/src/application/Program.java
@@ -1,18 +1,51 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class Program {
     public static void main(String[] args) {
-        String[] vect = new String[] {"Maria", "Bob", "Alex"};
+        List<String> list = new ArrayList<>();
+    
+        list.add("Maria");
+        list.add("Alex");
+        list.add("Bob");
+        list.add("Anna");
+        list.add(2, "Marco");
 
-        for(int i=0; i <vect.length; i++){
-            System.out.println(vect[i]);
+
+        System.out.println(list.size());
+
+        list.remove(1);
+
+        for(String x : list){
+            System.out.println(x);
         }
 
-        System.out.println("-------------");
+        System.out.println("------------");
+        list.removeIf(x -> x.charAt(0) == 'M');
 
-        for(String obj : vect){
-            System.out.println(obj);
+        for(String x : list){
+            System.out.println(x);
         }
+
+        System.out.println("------------");
+        System.out.println("Index of Bob: " + list.indexOf("Bob"));
+        System.out.println("Index of Marco: " + list.indexOf("Marco"));
+        System.out.println("------------");
+
+        List<String> result = list.stream().filter(x -> x.charAt(0) == 'A').collect(Collectors.toList());
+
+        for(String x : result){
+            System.out.println(x);
+        }
+        System.out.println("------------");
+
+        String name = list.stream().filter(x -> x.charAt(0) == 'A').findFirst().orElse(null);
+        System.out.println(name);
+
+
     }
 }
