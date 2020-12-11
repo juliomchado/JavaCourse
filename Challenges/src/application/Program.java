7f53766a8cbbc847794ce1c8c97f4cd88b4743b9
Julio Machado <juliocarlos00@hotmail.com>
2020-12-11 09:47:54 -0300

    list challenge

--- Challenges/src/application/Program.java
+++ Challenges/src/application/Program.java
@@ -1,34 +1,56 @@
 package application;
 
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
-import entities.Students;
+
+import entities.Employee;
 
 public class Program {
     public static void main(String[] args) throws ParseException {
         Scanner sc = new Scanner(System.in);
-        Students[] vect = new Students[10];
-        System.out.print("How many rooms will be Studentsed? ");
+        
+        System.out.print("How many employees will be registered? ");
         int n = sc.nextInt();
-        for (int i = 1; i <= n; i++) {
+
+        List<Employee> list = new ArrayList<>();
+
+
+        for(int i = 0; i < n; i ++){
             System.out.println();
-            System.out.println("Students #" + i + ":");
+            System.out.println("Employee #" + (i + 1) + ":");
+            System.out.print("Id: ");
+            Integer id = sc.nextInt();
             System.out.print("Name: ");
             sc.nextLine();
             String name = sc.nextLine();
-            System.out.print("Email: ");
-            String email = sc.nextLine();
-            System.out.print("Room: ");
-            int room = sc.nextInt();
-            vect[room] = new Students(name, email);
+            System.out.print("Salary: ");
+            Double salary = sc.nextDouble();
+
+            Employee emp = new Employee(id, name, salary);
+                
+            list.add(emp);
+        
         }
-        System.out.println();
-        System.out.println("Busy rooms:");
-        for (int i = 0; i < 10; i++) {
-            if (vect[i] != null) {
-                System.out.println(i + ": " + vect[i]);
-            }
+
+        System.out.println("Enter the employee id that will have salary increase: ");
+        int employeeId = sc.nextInt();
+
+        Employee id = list.stream().filter(x -> x.getId() == employeeId).findFirst().orElse(null);
+        if(id == null){
+            System.out.println("This id does not exist!");
         }
+        else{
+            System.out.print("Enter the percentage: ");
+            Double percentage = sc.nextDouble();
+            id.increaseSalary(percentage);
+            System.out.println("List of employees");
+            for(Employee emp : list){
+                System.out.println(emp);
+            } 
+        }
+
         sc.close();
     }
 }
