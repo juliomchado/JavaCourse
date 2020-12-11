c4737b4a87c90dfbde9e8ee6038574d5585daed7
Julio Machado <juliocarlos00@hotmail.com>
2020-12-11 08:58:11 -0300

    foreach

--- Application/src/application/Program.java
+++ Application/src/application/Program.java
@@ -1,34 +1,18 @@
 package application;
 
-import java.util.Locale;
-import java.util.Scanner;
-import entities.Product;
 
 public class Program {
     public static void main(String[] args) {
-        Locale.setDefault(Locale.US);
-        Scanner sc = new Scanner(System.in);
+        String[] vect = new String[] {"Maria", "Bob", "Alex"};
 
-        int n = sc.nextInt();
-        Product[] vect = new Product[n];
-
-        for (int i = 0; i < vect.length; i++) {
-            sc.nextLine();
-            String name = sc.nextLine();
-            double price = sc.nextDouble();
-            vect[i] = new Product(name, price);
+        for(int i=0; i <vect.length; i++){
+            System.out.println(vect[i]);
         }
 
-        double sum = 0;
-        
-        for(int i = 0; i< vect.length;i++){
-            sum += vect[i].getPrice();
+        System.out.println("-------------");
+
+        for(String obj : vect){
+            System.out.println(obj);
         }
-
-        double avg = sum / vect.length;
-
-        System.out.printf("AVERAGE PRICE = %.2f%n",avg);
-
-        sc.close();
     }
 }
