cbd339d7d2d93efc10e4b01987567696083f9ce0
Julio Machado <juliocarlos00@hotmail.com>
2020-12-12 17:46:58 -0300

    Inheritance

--- Application/src/application/Program.java
+++ Application/src/application/Program.java
@@ -3,25 +3,15 @@ package application;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
-import entities.Comment;
-import entities.Post;
+import entities.BusinessAccount;
 
 public class Program {
     public static void main(String[] args) throws ParseException {
 
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        BusinessAccount account = new BusinessAccount();
 
-        Comment c1 = new Comment("Have a nice trip!");
-        Comment c2 = new Comment("Wowo that's awesome!");
+        account.deposit(3);
 
-        Post p1 = new Post(sdf.parse("21/06/2018 13:05:44"), 
-        "Traveling to New Zealand", 
-        "I'm going to visit this wonderful contry!", 
-        12);
-
-        p1.addComment(c1);
-        p1.addComment(c2);
         
-        System.out.println(p1);
     }
 }
---
+++ Application/src/entities/BusinessAccount.java
@@ -0,0 +1,29 @@
+package entities;
+
+public class BusinessAccount extends Account {
+    public Double loanLimit;
+
+    public BusinessAccount() {
+        super();
+    }
+
+    public BusinessAccount(Integer number, String holder, Double balance, Double loadnLimit) {
+        super(number, holder, balance);
+        this.loanLimit = loadnLimit;
+    }
+
+    public Double getLoanLimit() {
+        return this.loanLimit;
+    }
+
+    public void setLoanLimit(Double loanLimit) {
+        this.loanLimit = loanLimit;
+    }
+
+    public void loan(double amount) {
+        if(amount <= loanLimit){
+            balance += amount - 10.0;
+        }
+    }
+
+}
