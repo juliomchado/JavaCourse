d4001d1679d4bbf58724ae4e99f8ea35c17bd142
Julio Machado <juliocarlos00@hotmail.com>
2020-12-12 19:11:38 -0300

    abstract classes

--- Application/src/application/Program.java
+++ Application/src/application/Program.java
@@ -1,6 +1,8 @@
 package application;
 
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
 
 import entities.Account;
 import entities.BusinessAccount;
@@ -9,14 +11,28 @@ import entities.SavingsAccount;
 public class Program {
     public static void main(String[] args) throws ParseException {
 
-        Account x = new Account(1020, "Alex", 1000.0);
-        Account y = new SavingsAccount(1023, "Maria", 1000.0, 0.01);
+        List<Account> list = new ArrayList<>();
 
-        x.withdraw(50.0);
-        y.withdraw(50.0);
-
-        System.out.println(x.getBalance());
-        System.out.println(y.getBalance());
+        list.add(new SavingsAccount(1001, "Alex", 500.00, 0.01));
+        list.add(new BusinessAccount(1002, "Maria", 1000.00, 400.0));
+        list.add(new SavingsAccount(1004, "Bob", 300.00, 0.01));        
+        list.add(new BusinessAccount(1005, "Anna", 500.00, 500.0));
         
+        double sum = 0.0;
+
+        for(Account acc : list){
+            sum += acc.getBalance();
+        }
+
+        System.out.printf("Total balance: %.2f%n", sum);
+
+        for(Account acc : list){
+            acc.deposit(10.0);
+        }
+        
+        for(Account acc : list){
+            System.out.printf("Updated balance for account %d: %.2f%n", acc.getNumber(), acc.getBalance());
+        }
+    
     }
 }
