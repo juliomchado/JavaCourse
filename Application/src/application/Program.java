88170b30c69b834baf1903bc93046bcff167f06c
Julio Machado <juliocarlos00@hotmail.com>
2020-12-12 20:03:53 -0300

    try catch

--- Application/src/application/Program.java
+++ Application/src/application/Program.java
@@ -2,14 +2,10 @@ package application;
 
 import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
-
-import entities.Shape;
-import entities_enums.Color;
-import entities.Rectangle;
-import entities.Circle;
 
 public class Program {
     public static void main(String[] args) throws ParseException {
@@ -17,37 +13,20 @@ public class Program {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        List<Shape> list = new ArrayList<>();
-
-        System.out.print("Enter the number of shapes: ");
-        int n = sc.nextInt();
-
-        for (int i = 1; i <= n; i++) {
-            System.out.println("Shape #" + i + " data:");
-            System.out.print("Rectangle or Circle (r/c)? ");
-            char ch = sc.next().charAt(0);
-            System.out.print("Color (BLACK/BLUE/RED): ");
-            Color color = Color.valueOf(sc.next());
-
-            if (ch == 'r') {
-                System.out.print("Widht: ");
-                double width = sc.nextDouble();
-                System.out.print("Height: ");
-                double height = sc.nextDouble();
-                list.add(new Rectangle(color, width, height));
-            } else {
-                System.out.print("Radius: ");
-                double radius = sc.nextDouble();
-                list.add(new Circle(color, radius));
-            }
+        try{
+            String[] vect = sc.nextLine().split(" ");
+            int position = sc.nextInt(); 
+            System.out.println(vect[position]);
+        
         }
-
-        System.out.println();
-        System.out.println("SHAPE AREAS: ");
-
-        for (Shape shape : list) {
-            System.out.println(String.format("%.2f", shape.area()));
+        catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Invalid position!"); 
         }
+        catch(InputMismatchException e){
+            System.out.println("Input error");
+        }
+        System.out.println("End of program"); 
+
 
         sc.close();
 
