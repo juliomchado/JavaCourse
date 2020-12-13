685824d73ff6e6bd79d440c7b9990c89e2bc745e
Julio Machado <juliocarlos00@hotmail.com>
2020-12-13 18:20:20 -0300

    finally

--- Application/src/application/Program.java
+++ Application/src/application/Program.java
@@ -1,49 +1,25 @@
 package application;
 
-import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.InputMismatchException;
-import java.util.List;
-import java.util.Locale;
+import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Program {
-    public static void main(String[] args) throws ParseException {
-
-        Locale.setDefault(Locale.US);
-
-        method1();
-
-        System.out.println("End of program");
-
-    }
-
-    public static void method1() {
-        System.out.println("***METHOD1 START***");
-        method2();
-        System.out.println("***METHOD1 END***");
-
-    }
-
-    public static void method2() {
-        System.out.println("***METHOD2 START***");
-        Scanner sc = new Scanner(System.in);
-
+    public static void main(String[] args) {
+        File file = new File("C:\\temp\\in.txt");
+        Scanner sc = null;
         try {
-            String[] vect = sc.nextLine().split(" ");
-            int position = sc.nextInt();
-            System.out.println(vect[position]);
-
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Invalid position!");
-            e.printStackTrace();
-            sc.next();
-        } catch (InputMismatchException e) {
-            System.out.println("Input error");
+            sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+            }
+        } catch (IOException e) {
+            System.out.println("Error opening file: " + e.getMessage());
+        } finally {
+            if (sc != null) {
+                sc.close();
+            }
+            System.out.println("Finally block executed");
         }
-
-        sc.close();
-        System.out.println("***METHOD2 END***");
-
     }
 }
