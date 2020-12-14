0dbc5fe685f8fb2a5fbe23ce09375dd07ecd4b7f
Julio Machado <juliocarlos00@hotmail.com>
2020-12-14 09:32:25 -0300

    segenerics

--- Application/src/application/Program.java
+++ Application/src/application/Program.java
@@ -1,22 +1,31 @@
 package application;
 
 import java.text.ParseException;
+import java.util.Locale;
+import java.util.Scanner;
 
-import model.entities.AbstractShape;
-import model.entities.Circle;
-import model.entities.Rectangle;
-import model.enums.Color;
+import services.PrintService;
 
 public class Program {
     public static void main(String[] args) throws ParseException {
 
-        AbstractShape s1 = new Circle(Color.BLACK, 2.0);
-        AbstractShape s2 = new Rectangle(Color.WHITE, 3.0, 4.0);
+        Locale.setDefault(Locale.US);
 
-        System.out.println("Circle color: " + s1.getColor());
-        System.out.println("Circle area: " + String.format("%.3f", s1.area()));
-        System.out.println("Rectangle color: " + s2.getColor());
-        System.out.println("Rectangle area: " + String.format("%.3f", s2.area()));
+        Scanner sc = new Scanner(System.in);
+        PrintService<Integer> ps = new PrintService<>();
 
+        System.out.print("How many values? ");
+
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int value = sc.nextInt();
+            ps.addValue(value);
+        }
+
+        ps.print();
+        System.out.println("First: " + ps.first());
+
+        sc.close();
     }
 }
