323d7be98ce38cd59f972d37f66d52d2ff62f0b7
Julio Machado <juliocarlos00@hotmail.com>
2020-12-31 16:43:15 -0300

    lambdas with consumer

---
+++ Application/Lambdas/Consumer function/src/application/Program.java
@@ -0,0 +1,35 @@
+package application;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+import entities.Product;
+import util.PriceUpdate;
+
+public class Program {
+    public static void main(String[] args) {
+
+        List<Product> list = new ArrayList<>();
+        list.add(new Product("Tv", 900.00));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.50));
+        list.add(new Product("HD Case", 80.90));
+
+        // list.forEach(new PriceUpdate());
+        // list.forEach(Product::staticPriceUpdate);
+        // list.forEach(Product::nonStaticPriceUpdate);
+        // Consumer<Product> cons = p -> p.setPrice(p.getPrice() * 1.1);
+        // list.forEach(cons);
+
+        list.forEach(x -> x.setPrice(x.getPrice() * 1.1));
+
+        list.forEach(System.out::println);
+
+    }
+}
